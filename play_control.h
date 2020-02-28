2fbdb6490a5c99ef6bd31446b7ee5aca9406a5ee
cleanli <clean_li@yahoo.com>
2020-02-29 03:46:30 +0800

    v0.2

--- play_control.h
+++ play_control.h
@@ -1,7 +1,11 @@
 #ifndef _PLAY_CONTROL
 #define _PLAY_CONTROL
 
-#define VERSION "0.1"
+/*
+ * 0.1 local mode works
+ * 0.2 net mode works
+ * */
+#define VERSION "0.2 net mode works"
 #define PORT_NUM 34567
 enum RUN_MODE
 {
