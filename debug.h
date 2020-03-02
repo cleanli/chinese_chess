33448e14b351b5c924e3ec60d8898098ca42d548
cleanli <clean_li@yahoo.com>
2020-03-02 21:26:05 +0800

    add process id in log

--- debug.h
+++ debug.h
@@ -3,15 +3,16 @@
 
 #include "windows.h"
 extern char debug_buf[1024];
-#define LEN_TIMESTAMP 22
+#define LEN_TIMESTAMP 28
 #define df(fmt,arg...) \
     { \
         SYSTEMTIME time;\
         GetLocalTime(&time);\
         char* full_str = debug_buf + LEN_TIMESTAMP;\
-        sprintf(debug_buf, "%d%02d%02d-%02d:%02d:%02d.%03d ", \
+        sprintf(debug_buf, "%d%02d%02d-%02d:%02d:%02d.%03d %05d ", \
                 time.wYear, time.wMonth, time.wDay, time.wHour, \
-                time.wMinute, time.wSecond, time.wMilliseconds);\
+                time.wMinute, time.wSecond, time.wMilliseconds, \
+                GetCurrentProcessId());\
         sprintf(full_str, fmt"\n", ##arg);\
         printf("%s", debug_buf);\
         fflush(stdout); \
